ee868d47d3cea7c60833f51eac221ebce704f2ad
Moraru <tiltiobusiness@gmail.com>
2022-06-09 15:33:37 +0300

    initial commit

---
+++ src/i18n/i18n.constants.ts
@@ -0,0 +1,16 @@
+export const namespaces = {
+    pages: {
+        hello: "pages.hello",
+        banner: "pages.banner",
+        about: "pages.about",
+        menu: "pages.menu"
+    },
+    common: "common",
+    header: "header",
+    footer: "footer"
+};
+
+export const languages = {
+    ro: "ro",
+    ru: "ru"
+};
